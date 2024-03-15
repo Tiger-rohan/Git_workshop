06b5c30385b08883b7604ed1473adce74bac41ac
J Naveen Raaj <naveen.raaj@tigeranalytics.com>
2024-03-15 14:59:10 +0530

    issue-1-resolved

--- simple_functions.py
+++ simple_functions.py
@@ -1,7 +1,25 @@
 # Custom python functions
-
+"""
+Custom Python Functions
 def double_number(a):
+This module has two custom Python functions:
+1. double_number(a): 
+   Description: This function takes a numeric input 'a' and returns its double.
+   Parameters:
+      - a : The input number.
+   Returns:
+      - double of 'a'
+"""
+def double_number(a):    
     return a+a
 
+"""
+2. square_number(a):
+   Description: This function takes a numeric input 'a' and returns its square.
+   Parameters:
+      - a : The input number.
+   Returns:
+      - square of 'a' 
+"""
 def square_number(a):
     return a*a
